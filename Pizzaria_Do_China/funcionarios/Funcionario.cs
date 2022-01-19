4b3035b2e03fb99465bcdff9b103dbc61734539a
brendo.freitas <brendofreitas.s@hotmail.com>
2022-01-19 16:45:14 -0300

    Adicionando Funcionarios

---
+++ Pizzaria_Do_China/funcionarios/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzaria_Do_China.funcionarios
+{
+    public class Funcionario
+    {
+        private string Nome;
+        private int Cpf;
+
+        public Funcionario(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Funcionario(int cpf)
+        {
+            Cpf = cpf;
+        }
+    }
+}
